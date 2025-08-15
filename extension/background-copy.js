a609306506eb14ea0af04899daede8c1f63a09c5
ccmmail <ccmmail@gmail.com>
2025-08-14 23:41:19 -0700

    Squashed 'BrowserExtension/' changes from 631a041..e70d16f
    e70d16f refactor: adopt modern side panel approach
    
    git-subtree-dir: BrowserExtension
    git-subtree-split: e70d16f6cc52d7ea94dc2b5c570e6c2e1bf95a09

--- extension/background-copy.js
+++
@@ -1,9 +0,0 @@
-const chrome = window.chrome
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("AI Recruiting Agent extension installed")
-})
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, { action: "togglePanel" })
-})
